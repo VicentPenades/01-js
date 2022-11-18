5356301475b2120af91fe1c5032ee4503166182a
Vicent Penadés <vicent.penades@idrica.com>
2022-11-18 18:02:59 +0100

    04-hidden-search-widget-v1

---
+++ 04-hidden-search-widget/index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button1");
+const input1 = document.querySelector(".input");
+
+button1.addEventListener("click", (input) => {
+  input1.classList.add("input-closed");
+});
